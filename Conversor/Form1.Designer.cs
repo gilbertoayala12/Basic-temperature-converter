c697be5853836cceffa4881ab690b88f1ee080db
gilberto ayala <gilbertoayala12@gmail.com>
2017-02-15 19:14:20 -0700

    pushing everything :godmode:

---
+++ Conversor/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Conversor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioK = new System.Windows.Forms.RadioButton();
+            this.radioC = new System.Windows.Forms.RadioButton();
+            this.radioF = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(96, 121);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Transformar";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(35, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "°F";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(5, 58);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(36, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "°C";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(5, 81);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(36, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "°K";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioK);
+            this.groupBox2.Controls.Add(this.radioC);
+            this.groupBox2.Controls.Add(this.radioF);
+            this.groupBox2.Location = new System.Drawing.Point(188, 46);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(96, 121);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "A";
+            // 
+            // radioK
+            // 
+            this.radioK.AutoSize = true;
+            this.radioK.Location = new System.Drawing.Point(5, 81);
+            this.radioK.Name = "radioK";
+            this.radioK.Size = new System.Drawing.Size(36, 17);
+            this.radioK.TabIndex = 2;
+            this.radioK.TabStop = true;
+            this.radioK.Text = "°K";
+            this.radioK.UseVisualStyleBackColor = true;
+            this.radioK.CheckedChanged += new System.EventHandler(this.radioK_CheckedChanged);
+            // 
+            // radioC
+            // 
+            this.radioC.AutoSize = true;
+            this.radioC.Location = new System.Drawing.Point(5, 58);
+            this.radioC.Name = "radioC";
+            this.radioC.Size = new System.Drawing.Size(36, 17);
+            this.radioC.TabIndex = 1;
+            this.radioC.TabStop = true;
+            this.radioC.Text = "°C";
+            this.radioC.UseVisualStyleBackColor = true;
+            this.radioC.CheckedChanged += new System.EventHandler(this.radioC_CheckedChanged);
+            // 
+            // radioF
+            // 
+            this.radioF.AutoSize = true;
+            this.radioF.Location = new System.Drawing.Point(6, 35);
+            this.radioF.Name = "radioF";
+            this.radioF.Size = new System.Drawing.Size(35, 17);
+            this.radioF.TabIndex = 0;
+            this.radioF.TabStop = true;
+            this.radioF.Text = "°F";
+            this.radioF.UseVisualStyleBackColor = true;
+            this.radioF.CheckedChanged += new System.EventHandler(this.radioF_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Temp Inicial";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 214);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(104, 285);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Temp Convertida";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Convertir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 342);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioK;
+        private System.Windows.Forms.RadioButton radioC;
+        private System.Windows.Forms.RadioButton radioF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Conversor/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversor
+{
+    public partial class Form1 : Form
+    {
+        public double tempInicial;
+        public double tempFinal;
+        public char tipo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                radioF.Checked = false;
+                radioF.Enabled = false;
+                radioK.Enabled = true;
+                radioC.Enabled = true;
+                textBox2.Clear();
+            }
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="")
+            {
+                MessageBox.Show("El campo de texto está vacio");
+                textBox1.Focus();
+            }
+            else
+            {
+                if (radioButton1.Checked)
+                {
+                    tipo = 'f';
+
+                }
+                else if (radioButton2.Checked)
+                {
+                    tipo = 'c';
+                }
+                else if(radioButton3.Checked)
+                {
+                    tipo = 'k';
+                }
+                try
+                {
+                    tempInicial = double.Parse(textBox1.Text);
+                    switch (tipo)
+                    {
+                        case 'c':
+                            if (radioF.Checked)
+                            {
+                                tempFinal = ((tempInicial * (1.8)) + 32);
+                                textBox2.Text = tempFinal + " °F";
+                            }
+                            else if (radioK.Checked)
+                            {
+                                tempFinal = ((tempInicial + 273.15));
+                                textBox2.Text = tempFinal + " °K";
+                            }
+                            break;
+
+                        case 'f':
+                            if (radioC.Checked)
+                            {
+                                tempFinal = (tempInicial - 32) / 1.8;
+                                textBox2.Text = tempFinal.ToString("#.####") + " °C";
+                            }
+                            else if(radioK.Checked)
+                            {
+                                tempFinal = ((tempInicial - 32) / 1.8) + 273.15;
+                                textBox2.Text = tempFinal.ToString("#.####") + " °K";
+                            }
+                            break;
+                        case 'k':
+                            if (radioC.Checked)
+                            {
+                                tempFinal = tempInicial - 273.15;
+                                textBox2.Text = tempFinal + " °C";
+                            }
+                            else if (radioF.Checked)
+                            {
+                                tempFinal = (1.8 * (tempInicial - 273.15)) + 32;
+                                textBox2.Text = tempFinal + " °F";
+                            }
+                            break;
+                    }
+                }
+                catch (FormatException )
+                {
+                    MessageBox.Show("El texto introducido es invalido, introduce un numero valido");
+                    textBox1.Clear();
+                    textBox1.Focus();
+
+                    
+                }
+                
+                
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                radioC.Checked = false;
+                radioF.Enabled = true;
+                radioK.Enabled = true;
+                radioC.Enabled = false;
+                textBox2.Clear();
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                radioK.Checked = false;
+                radioF.Enabled = true;
+                radioK.Enabled = false;
+                radioC.Enabled = true;
+                textBox2.Clear();
+
+            }
+        }
+
+        private void radioF_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+
+        private void radioC_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+
+        private void radioK_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+    }
+}
